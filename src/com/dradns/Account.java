e59cc1f773c81303abd12e20b7d04574ff040858
dradns <dradns@gmail.com>
2022-04-13 07:17:20 +0300

    second

--- src/com/dradns/Account.java
+++ src/com/dradns/Account.java
@@ -1,5 +1,20 @@
 package com.dradns;
 
 public class Account {
-    public float balance;
+    private float balance;
+
+    public void deposit(float amount){
+        if(amount >= 0)
+            this.balance = this.balance + amount;
+    }
+
+    public void withdraw(float amount){
+        if(amount >= 0)
+            this.balance -= amount;
+    }
+
+    public float getBalance(){
+        System.out.println(balance);
+        return 0;
+    }
 }
--- src/com/dradns/Main.java
+++ src/com/dradns/Main.java
@@ -3,8 +3,10 @@ package com.dradns;
 public class Main {
 
     public static void main(String[] args){
-        User user = new User("DraD",30);
-        //user.name = "DraD";
-        user.sayHello();
+        var account = new Account();
+        account.deposit(10);
+        account.getBalance();
+        account.withdraw(5);
+        account.getBalance();
     }
 }
